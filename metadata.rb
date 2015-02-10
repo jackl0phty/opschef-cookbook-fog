f8dd4e0e90a46a5c9f1f6dc09ac7811d86917ce1
Dan Thagard <dthagard@qqsolutions.com>
2015-02-10 09:38:37 -0500

    Updated fog install version
    - Fixed hard-coded reference to specific FOG version in default recipe.
    - Updated default attributes to use latest FOG version.

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "jackl0phty@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures FOG"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 
 %w( mysql ).each { |cb| depends cb }
--- recipes/default.rb
+++ recipes/default.rb
@@ -11,7 +11,7 @@ script "download-untar-fog" do
   user "root"
   cwd "/opt"
   code <<-EOH
-  wget http://downloads.sourceforge.net/project/freeghost/FOG/fog_0.32/fog_#{node['fog']['version']}.tar.gz
+  wget http://downloads.sourceforge.net/project/freeghost/FOG/fog_#{node['fog']['version']}/fog_#{node['fog']['version']}.tar.gz
   tar zxvhf fog_#{node['fog']['version']}.tar.gz
   rm -f fog_#{node['fog']['version']}.tar.gz
   EOH
@@ -19,15 +19,32 @@ script "download-untar-fog" do
   not_if "test -d /opt/fog_#{node['fog']['version']}"
 end
 
+# Install expect
+package "expect" do
+  action :install
+end
+
+# Disable IP tables
+service "iptables" do
+  action :disable
+end
+service "ip6tables" do
+  action :disable
+end
+
 # Install apache2 & php5
 case node['platform_family']
- 
+
   when "debian"
     %w{ apache2 php5 php5-gd php5-cli php5-mysql php5-curl
-        isc-dhcp-server tftpd-hpa tftp-hpa nfs-kernel-server 
+        isc-dhcp-server tftpd-hpa tftp-hpa nfs-kernel-server
         vsftpd net-tools wget xinetd sysv-rc-conf tar gzip
-        cpp gcc g++ m4 htmldoc perl libcrypt-passwdmd5-perl 
+        cpp gcc g++ m4 htmldoc perl libcrypt-passwdmd5-perl
         lftp ssh php-gettext clamav-freshclam }.each do |pkg|
+    package pkg
+  end
+  when "centos"
+    %w{ httpd php php-mysql tar gzip libgcc  }.each do |pkg|
     package pkg
   end
 end
